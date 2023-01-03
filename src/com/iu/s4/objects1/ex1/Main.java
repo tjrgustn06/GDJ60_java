312aac8640f51e7fc0961605e3d93c3d2720e47d
tjrgustn06 <tjrgustn06@naver.com>
2023-01-03 14:47:04 +0900

    1

--- src/com/iu/s4/objects1/ex1/Main.java
+++ src/com/iu/s4/objects1/ex1/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("학원에서 공부한 내용");
 		System.out.println("start");
 		
 		MenuController menuController = new MenuController();	
