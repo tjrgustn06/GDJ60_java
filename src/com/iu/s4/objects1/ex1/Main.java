7a4ef4da433728f28399c6640f962f716b457c9d
tjrgustn06 <tjrgustn06@naver.com>
2023-01-03 14:18:41 +0900

    test

--- src/com/iu/s4/objects1/ex1/Main.java
+++ src/com/iu/s4/objects1/ex1/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("start");
 		
-		MenuController menuController = new MenuController();
+		MenuController menuController = new MenuController();	
 		
 		menuController.start();
 	}
